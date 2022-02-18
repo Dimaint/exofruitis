a704700c18da8a8046d87937dcb9dea763c989bc
Dimaint <s_aint@list.ru>
2022-02-19 00:00:18 +0600

    order form layout

---
+++ src/components/cart/order-form.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-btn class="white--text mt-10" color="teal" v-show="!overlay" @click="overlay = !overlay">
+        Заказать
+      </v-btn>
+
+      <v-overlay :z-index="zIndex" :value="overlay" >
+        <v-container>
+          <v-row >
+            <v-col cols="12" lg="3">
+              <v-text-field
+                v-model="firstname"
+                :rules="nameRules"
+                label="Имя"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" lg="3">
+                <v-text-field
+                v-model="phoneNumber"
+                :counter="9"
+                maxlength="9"
+                :error-messages="errors"
+                label="Phone Number"
+                required
+                ></v-text-field>
+            </v-col>
+            <v-col cols="12" lg="6">
+              <v-text-field
+                v-model="street"
+                :rules="steetRules"
+                :counter="30"
+                label="Улица"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-text-field
+                v-model="street"
+                :rules="steetRules"
+                :counter="10"
+                label="Дом(корпус, строение)"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-text-field
+                v-model="street"
+                :rules="steetRules"
+                :counter="10"
+                label="Подъезд"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-text-field
+                v-model="street"
+                :rules="steetRules"
+                :counter="10"
+                label="Этаж"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+                <v-text-field
+                v-model="comment"
+                color="teal"
+            >
+              <template v-slot:label>
+                <div>
+                  Комментарий к заказу <small>(optional)</small>
+                </div>
+              </template>
+            </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-card-actions class="mx-auto my-auto">
+              <v-btn color="secondary" text @click="increment">
+                Оформить
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn color="warning" text @click="overlay = false">
+                Отмена
+              </v-btn>
+            </v-card-actions>
+          </v-row>
+        </v-container>
+      </v-overlay>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  data: () => ({
+    overlay: false,
+    zIndex: 0,
+  }),
+  methods: {
+    // ...mapActions([
+    // ]),
+  },
+  computed: {
+    ...mapGetters(["CART"]),
+  },
+};
+</script>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,44 +1,51 @@
 <template>
-    <v-container class="d-flex justify-space-around flex-wrap">
-        
-
-        <cart-item
-          v-for="(product, index) in CART"
-          :key="product.id"
-          :product_data="product"
-          @deleteFromCart="deleteFromCart(index)"
-          @decrement="decrement(index)"
-          @increment="increment(index)"
+  <v-container class="d-flex justify-space-between flex-column">
+    <v-row class="d-flex justify-space-around flex-wrap">
+      <cart-item
+        v-for="(product, index) in CART"
+        :key="product.id"
+        :product_data="product"
+        @deleteFromCart="deleteFromCart(index)"
+        @decrement="decrement(index)"
+        @increment="increment(index)"
       />
-
-    </v-container>
+    </v-row>
+    <v-row>
+      <order-form />
+    </v-row>
+  </v-container>
 </template>
 
 
 <script>
-import {mapActions, mapGetters} from 'vuex'
-import cartItem from '../components/cart/cart-item.vue'
+import { mapActions, mapGetters } from "vuex";
+import cartItem from "../components/cart/cart-item.vue";
+import OrderForm from "../components/cart/order-form.vue";
 export default {
   components: {
-    cartItem
+    cartItem,
+    OrderForm,
   },
-    methods: {
-      ...mapActions(['DELETE_FROM_CART', 'INCREMENT_CART_ITEM', 'DECREMENT_CART_ITEM']),
+  methods: {
+    ...mapActions([
+      "DELETE_FROM_CART",
+      "INCREMENT_CART_ITEM",
+      "DECREMENT_CART_ITEM",
+    ]),
 
-      deleteFromCart(index) {
-        this.DELETE_FROM_CART(index);
-        },
-      increment(index) {
-          this.INCREMENT_CART_ITEM(index);
-        //   console.log(id)
-      },
-      decrement(index) {
-          this.DECREMENT_CART_ITEM(index);
-      }
+    deleteFromCart(index) {
+      this.DELETE_FROM_CART(index);
     },
-    computed: {
-      ...mapGetters(['CART'])
+    increment(index) {
+      this.INCREMENT_CART_ITEM(index);
+      //   console.log(id)
     },
-    
-}
+    decrement(index) {
+      this.DECREMENT_CART_ITEM(index);
+    },
+  },
+  computed: {
+    ...mapGetters(["CART"]),
+  },
+};
 </script>
